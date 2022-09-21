5abf598ac4bbae134ce5156f7ac018c4ee800217
Mini Sarin <mini.sarin@reece.com.au>
2022-09-21 15:29:36 +1000

    Testimonials video integration - not working yet

--- src/pages/testimonials.js
+++ src/pages/testimonials.js
@@ -14,6 +14,20 @@ import {
     playButton,
 } from "./testimonials.module.css";
 
+const Video = ({ videoSrcURL, videoTitle, ...props }) => (
+  <div className={videoCard}>
+    <iframe
+      src={videoSrcURL}
+      title={videoTitle}
+      allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+      frameBorder="0"
+      webkitallowfullscreen="true"
+      mozallowfullscreen="true"
+      allowFullScreen
+    />
+  </div>
+)
+
 const Testimonials = () => {
     return (
         <div className={container}>
@@ -27,8 +41,15 @@ const Testimonials = () => {
                     </div>    
                     <div className={videoSection}>
                         <div className={videoCard}>
+                            {/* <video className={videoImage} thumbnail= "../images/thumbnails/testimonial-RodBerger.png" controls>
+                                <source src={clientSpeakVideo1} type="video/mp4"/>
+                            </video> */}
                             <div className={videoImage}><StaticImage alt="Client testimonial 1" src="../images/thumbnails/testimonial-RodBerger.png"/></div>
                             <div className={playButton}></div>
+                            <div>
+                            <Video videoSrcURL="https://www.youtube.com/embed/Kxw2OjX0B10"
+                                    videoTitle="Some Video"/>
+                            </div>          
                         </div>
                         <div className={videoCard}>
                             <div className={videoImage}><StaticImage alt="Client testimonial 2" src="../images/thumbnails/testimonial-CJ.png"/></div>
@@ -38,6 +59,7 @@ const Testimonials = () => {
                             <div className={videoImage}><StaticImage alt="Client testimonial 3" src="../images/thumbnails/testimonial-GB.png"/></div>
                             <div className={playButton}></div>
                         </div>
+                        
                     </div>
                 </div>
             </div>
