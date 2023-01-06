c94ea2b5d562eb7abea1bd97f67cf6e7a4687bd7
Mini Sarin <mini.sarin@reece.com.au>
2023-01-06 15:53:07 +1100

    Added thank you page

--- src/pages/thankYou.js
+++ src/pages/thankYou.js
@@ -7,7 +7,14 @@ import {
     thankYouText,
     thankYouSubText,
     image,
+    contactBox,
+    contactSection,
+    contactText,
+    icon,
+    wrapperBottom
 } from "./thankYou.module.css";
+
+import {contactNumber, emailId, location} from "../resources/strings";
 
 const ThankYou = () => {
   return (
@@ -15,7 +22,23 @@ const ThankYou = () => {
         <div className={innerContainer}>
             <h3 className={thankYouText}>Thank You for reaching out.</h3>
             <p><span className={thankYouSubText}>We have received your e-mail and will get back to you as soon as we can.</span></p>
-            <div className={image}><StaticImage alt="Thank you" src="../../images/thankyou.svg"/></div>
+            <div className={image}><StaticImage alt="Thank you" src="../images/thankyou.svg"/></div>
+            <div className={wrapperBottom}>
+                <div className={contactSection}>
+                    <div className={contactBox}>
+                        <StaticImage className={icon} alt="Call Icon" src="../../images/call 1.png"></StaticImage>
+                        <p className={contactText}>{contactNumber}</p>
+                    </div>
+                    <div className={contactBox}>
+                        <StaticImage className={icon} alt="Mail Icon" src="../../images/mail icon.svg"></StaticImage>
+                        <p className={contactText}>{emailId}</p>
+                    </div>
+                    <div className={contactBox}>
+                        <StaticImage className={icon} alt="Location Icon" src="../../images/location icon.svg"></StaticImage>
+                        <p className={contactText}>{location}</p>
+                    </div>
+                </div>
+            </div>
         </div>
 
     </div>
